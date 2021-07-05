0113260ba30f3b41910c0c4defc99fdc73b6812e
Jim <jdhankee@yahoo.com>
2021-07-05 10:28:39 -0400

    Revert "Merge pull request #1 from jhankee/JohnHolidayUpdates_0703"
    This reverts commit e0edc7f36d3813bd225a57445dcfc32f06aba2af, reversing
    changes made to 23701a361077a143f613e2ebf087ae15c7569a50.

---
+++ documentation/index creation.sql
@@ -0,0 +1,2 @@
+CREATE INDEX `coach_has_clearance_idx` ON leaguetracker`.`coach` (`hasClearances` ASC) VISIBLE;
+CREATE INDEX `invoice_paidStatus_idx` ON `leaguetracker`.`invoice` (`paidStatus` ASC) VISIBLE;
